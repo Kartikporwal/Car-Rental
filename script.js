c7ca32404ead1641102b88ef1fcaac405f4a8bda
kartikPorwal14 <porwalkartik14@gmail.com>
2025-05-29 14:12:55 +0530

    Project Upload

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar')
 
 menu.onclick = () => {
     menu.classList.toggle("move");
+    navbar.classList.toggle('open-menu');
+
 };
+window.onscroll = () => {
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+};
+
+window.onload = () => {
+    let today = new Date().toISOString().split('T')[0];
+    document.getElementById('start-date').value = today;
+    document.getElementById('return-date').value = new Date(
+        Date.now() + 7 * 86400000
+    )
+    .toISOString()
+    .split('T')[0];
+}
